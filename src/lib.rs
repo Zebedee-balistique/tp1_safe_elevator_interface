f62cd44f5ae16a0de1547d0154660a912ea14d65
esmelero <esmelero@vm-esmelero.local.isima.fr>
2026-02-24 23:06:41 +0100

    Fin TP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum State {
     Idle,
@@ -14,6 +12,7 @@ pub enum ElevatorError {
     InvalidFloor(i32),
     CannotOpenWhileMoving,
     DoorsAlreadyClosed,
+    EmptyQueue,
 }
 
 pub struct Elevator {
@@ -29,81 +28,106 @@ pub struct Status {
 }
 
 impl Elevator {
-
     pub fn new(nfloor: i32) -> Result<Elevator, String> {
-        
-        if nfloor < 0 || nfloor > 5
-        {
-            return Err("invalid start floor".to_string());
-        }
-        else {
-            Ok(Self { floor: nfloor , queue: Vec::new(), state: State::DoorsOpen})
+        if !(0..=5).contains(&nfloor) {
+            Err("invalid start floor".to_string())
+        } else {
+            Ok(Self {
+                floor: nfloor,
+                queue: Vec::new(),
+                state: State::Idle,
+            })
         }
     }
 
-    
-    pub fn call(&mut self, nextfloor: i32) -> Result<String, ElevatorError>{
+    pub fn call(&mut self, nextfloor: i32) -> Result<String, ElevatorError> {
         match nextfloor {
-            d if d < 0 || d > 5 => return Err(ElevatorError::InvalidFloor(nextfloor)),
-            val if val == self.floor => return Err(ElevatorError::InvalidFloor(nextfloor)),
-            d if d < self.floor => {self.state = State::MovingDown; return Ok("call accepted".to_string())},
-            _ => {self.state = State::MovingUp; if Some(nextfloor) != self.queue.last().copied() {self.queue.push(nextfloor)}; return Ok("call accepted".to_string())}
+            d if !(0..=5).contains(&d) => Err(ElevatorError::InvalidFloor(nextfloor)),
+            val if val == self.floor => Ok("same floor accepter".to_string()),
+            d if d < self.floor => {
+                self.state = State::MovingDown;
+                if Some(&nextfloor) != self.queue.last() {
+                    self.queue.push(nextfloor)
+                };
+                Ok("call accepted".to_string())
+            }
+            _ => {
+                self.state = State::MovingUp;
+                if Some(&nextfloor) != self.queue.last() {
+                    self.queue.push(nextfloor)
+                };
+                Ok("call accepted".to_string())
+            }
         }
     }
 
     pub fn state(&self) -> State {
-        return self.state;
+        self.state
     }
 
     pub fn floor(&self) -> i32 {
-        return self.floor;
+        self.floor
     }
 
     pub fn queue(&self) -> Vec<i32> {
-        return self.queue.clone();
+        self.queue.clone()
     }
 
-    pub fn open_doors(&self) -> Result<String, ElevatorError> {
-        if self.state == State::DoorsOpen {
-            return Ok("doors open".to_string());
-        }
-        else {
-            return Err(ElevatorError::CannotOpenWhileMoving);
+    pub fn open_doors(&mut self) -> Result<String, ElevatorError> {
+        if self.state == State::Idle {
+            self.state = State::DoorsOpen;
+            Ok("doors open".to_string())
+        } else {
+            Err(ElevatorError::CannotOpenWhileMoving)
         }
     }
 
     pub fn close_doors(&mut self) -> Result<String, ElevatorError> {
-        if self.state != State::DoorsOpen {
-            return Ok("doors closed".to_string());
-        }
-        else {
-            self.state = State::Idle;
-            return Err(ElevatorError::DoorsAlreadyClosed);
+        if self.state == State::DoorsOpen {
+            if self.queue.is_empty() {
+                self.state = State::Idle;
+            } else {
+                if self.queue.first() < Some(&self.floor) {
+                    self.state = State::MovingDown;
+                    dbg!("Descente");
+                } else {
+                    self.state = State::MovingUp;
+                }
+            }
+            Ok("close doors".to_string())
+        } else {
+            Err(ElevatorError::DoorsAlreadyClosed)
         }
     }
 
     pub fn status(&self) -> Status {
-        return Status {floor: self.floor, state: self.state, queue: self.queue.clone()};
+        Status {
+            floor: self.floor,
+            state: self.state,
+            queue: self.queue.clone(),
+        }
     }
 
     pub fn step(&mut self) -> Result<String, ElevatorError> {
-        if self.state != State::Idle && self.state != State::DoorsOpen {
-            if self.state == State::MovingUp {
-                self.floor += 1;
-            }
-            else {
-                self.floor -= 1;
-            }
+        if self.state != State::DoorsOpen {
+            if !self.queue.is_empty() {
+                if self.state == State::MovingUp {
+                    self.floor += 1;
+                } else {
+                    self.floor -= 1;
+                }
 
-            if Some(self.floor) == self.queue.first().copied() {
-                self.state = State::DoorsOpen;
-                self.queue.remove(0);
+                if Some(self.floor) == self.queue.first().copied() {
+                    self.state = State::DoorsOpen;
+                    self.queue.remove(0);
+                }
+                Ok("step".to_string())
+
+            } else {
+                    Err(ElevatorError::EmptyQueue)
             }
-            return Ok("step".to_string());
-        }
-        else {
-            return Err(ElevatorError::CannotMoveDoorsOpen);
+        } else {
+            Err(ElevatorError::CannotMoveDoorsOpen)
         }
     }
-    
 }
